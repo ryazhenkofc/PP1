9359feeb10b73ed92d842929bc6a14d56424f722
Ten Dmitriy <dima.ten031@gmail.com>
2023-10-02 09:21:00 +0600

    Lab Update

---
+++ Lab 4/i.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(){
+    int n,m;
+    cin >> n >> m;
+    int arr[n][m];
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++){
+            cin >> arr[i][j];
+        }
+    }
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++){
+            int tmp = sqrt(arr[i][j]);
+            if (tmp * tmp == arr[i][j]){
+                arr[i][j] = tmp;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++){
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
