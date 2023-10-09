18baeec0fa5a44800d82bf60c097830b6fa51bb6
Ten Dmitriy <dima.ten031@gmail.com>
2023-10-09 09:17:49 +0600

    Lab Update

--- Lab 4/l.cpp
+++ Lab 4/l.cpp
@@ -1,33 +1,30 @@
 #include <iostream>
-#include <bitset>
-
+#include <climits>
 using namespace std;
 
 int main(){
     int n, m;
     cin >> n >> m;
     int arr[n][m];
-    
-
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
             cin >> arr[i][j];
         }
     } 
-    cout << "coordinates of min elements:";
-    int pos = 0, pos1 = 0, sum = 0;
+    cout << "coordinates of min elements:" << endl;
+    int pos1 = 0, pos2 = 0, sum = 0;
     for(int j = 0; j < m; j++){
-        int min = 999999999;
+        int min = INT_MAX;
         for(int i = 0; i < n; i++){
             if(arr[i][j] < min){
                 min = arr[i][j];
                 pos1 = i;
-                pos = j;
+                pos2 = j;
             }   
         }
-        cout << endl << pos1 + 1 << ";" << pos + 1;
+        cout << pos1 + 1 << ";" << pos2 + 1 << endl;
         sum += min;
     } 
-    cout << endl <<endl << "Their sum:" << endl << sum;
+    cout << endl << "Their sum:" << endl << sum;
     return 0;
 }
