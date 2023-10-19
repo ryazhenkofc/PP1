7e9ce4e0592057e0c9ff7b2e450c20b624663a38
Ten Dmitriy <dima.ten031@gmail.com>
2023-10-19 20:11:02 +0600

    Lab Update

--- Lab_5/a.cpp
+++ Lab_5/a.cpp
@@ -2,9 +2,21 @@
 
 using namespace std;
 
-int main(){
-    string n;
-    cin >> n;
-    
+int main() {
+    string s;
+    cin >> s;
+
+    int cap = 0;
+    int sml = 0;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] >= 'A' && s[i] <= 'Z') {
+        cap++;
+        } else if (s[i] >= 'a' && s[i] <= 'z') {
+        sml++;
+        }
+    }
+
+    cout << sml << " " << cap << endl;
+
     return 0;
 }
