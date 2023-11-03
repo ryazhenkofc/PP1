cb984d072ae716bf0a2c2d5568c5a8ee01b76243
Ten Dmitriy <dima.ten031@gmail.com>
2023-11-03 10:04:00 +0600

    Lab Update

--- Lab_9/g.cpp
+++ Lab_9/g.cpp
@@ -1,14 +1,33 @@
-#include <iostream>
-
-using namespace std;
-
-int main(){
-    string s;
-    cin >> s;
-    for(int i = 0; i < s.size(); i++){
-        if(i != s.size() - 1){
-            if(s[i] == '1' and s[i+1] == '1')
-        }
-    }
-    return 0;
+#include <iostream> 
+using namespace std; 
+int main(){ 
+    string s; 
+    cin >> s; 
+    string res; 
+    int count = 0; 
+    for(int i = 0; i < s.size(); i++){ 
+        if(s[i] == '0'){ 
+            cout << "0"; 
+            count = 0; 
+        } 
+        else{ 
+            for(int j = i; j < s.size(); j++){ 
+                if(s[j] == '1'){ 
+                    count++; 
+                } 
+                if(count == 2){ 
+                    count = 0; 
+                    i = j; 
+                    break; 
+                } 
+                if(count == 1 and s[j] != s[j+1] ){ 
+                    cout << "1"; 
+                    count = 0; 
+                    i = j; 
+                    break; 
+                } 
+            } 
+        } 
+    } 
+    return 0; 
 }
