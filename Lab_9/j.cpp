625bc5f8a53dbca45751c0e2b4ef75e361537dff
Ten Dmitriy <dima.ten031@gmail.com>
2023-11-06 09:42:06 +0600

    Lab Update

---
+++ Lab_9/j.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+
+void serial_counter(int n){
+        map<string, int> m;
+    for(int i = 0; i < n; i++){
+        string s;
+        int x;
+        cin >> s >> x;
+        m[s] = m[s] + x;
+    }
+
+    for(const auto &it : m){
+        cout << it.first << ' ' << it.second << endl;
+    }
+}
+
+int main(){
+    int n;
+    cin >> n;
+    serial_counter(n);
+
+    return 0;
+}
