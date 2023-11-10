208c727b889b0ca380583517718799c123ec4b8f
Ten Dmitriy <dima.ten031@gmail.com>
2023-11-10 10:18:55 +0600

    Lab Update

--- Lab_11/c.cpp
+++ Lab_11/c.cpp
@@ -1,21 +1,30 @@
 #include <iostream>
 #include <map>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
 int main(){
     int n;
     cin >> n;
-    map<string, int> m;
-    int total = 0;
+    map<string, double> m;
+    double total = 0;
     for(int i = 0; i < n; i++){
         string s;
-        int x;
+        double x;
         cin >> s >> x;
         m[s] += x;
         total += x;
     }
+    vector<pair<double, string>> v;
     for(auto x : m){
-        cout << x.first << ' ' << x.second << endl;
+        x.second = x.second / total * 100;
+        v.push_back(make_pair(x.second, x.first));
+    }
+    sort(v.rbegin(), v.rend());
+    for(auto x : v){
+        cout << x.second << ' ' << x.first << '%' << endl;
     }
     return 0;
 }
